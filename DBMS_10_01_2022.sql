058f1c6c306cffc6e59a2b6b662f6d58b850f252
Pranjal Pansuriya <>
2022-01-11 13:05:11 +0530

    Create DBMS_10_01_2022.sql

---
+++ DBMS_10_01_2022.sql
@@ -0,0 +1,39 @@
+create database Demo
+
+create table Student
+(
+	StudentID int PRIMARY KEY,
+	StudentName varchar(100),
+	Age int DEFAULT 18,
+	Email varchar(50),
+)
+
+Insert into Student values
+(1, 'Pranjal', 23, 'pranjal@gmail.com'),
+(2, 'Rushi', 20, 'rushi@aimdek.com'),
+(3, 'Dipam', 18, 'dipam@yahoo.com'),
+(4, 'Dev', 30, 'dev@gmail.com'),
+(5, 'Rishi', 10, 'rishi@gmail.com'),
+(6, 'Rahul', 18, 'rahul@yahoo.com')
+
+Select * from Student
+
+Select DISTINCT Age from Student
+
+Select * from Student where StudentName = 'Pranjal'
+
+Select * from Student where Email Like '%mail.com'
+
+Select * from Student Order by StudentName
+
+Select * from Student Order by StudentName Desc
+
+SELECT  * from Student WHERE Email Not Like '%gmail.com'
+
+Select Age,COUNT(*) from Student GROUP BY Age 
+
+Alter Table Student 
+Add MobileNo varchar(10)
+
+
+
